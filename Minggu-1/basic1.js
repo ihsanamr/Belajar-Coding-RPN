f5002492f7a3523d84e4618565f7b4dd4bc1ad7c
Ihsan Amri Muyassar <ihsan.muyassar@binus.ac.id>
2023-09-24 16:43:00 +0700

    belajar dasar js

---
+++ Minggu-1/basic1.js
@@ -0,0 +1,36 @@
+// cara membuat variabel
+var a = 'a';
+let b = 'b';
+const c = 'c';
+
+// variabel bisa menampung tipe data string (teks), number/integer, array, object, dan boolean
+let nama = "Ihsan"; //string
+let umur = 18; //number
+let apakahValid = true; //boolean
+let uang = 5000.5; //number
+
+// operator aritmatika
+let angka1 = 10;
+let angka2 = 20;
+console.log(angka1 + angka2);
+console.log(angka1 - angka2);
+console.log(angka1 / angka2);
+console.log(angka1 % angka2);
+console.log(angka1 * angka2);
+
+
+// disini belajar mengisi variabel
+let penampungHasilOperatorAritmatika = angka1 + angka2;
+console.log(penampungHasilOperatorAritmatika); //hasilnya 30
+
+penampungHasilOperatorAritmatika = angka1 - angka2;
+console.log(penampungHasilOperatorAritmatika); //hasilnya -10
+
+penampungHasilOperatorAritmatika = angka1 / angka2;
+console.log(penampungHasilOperatorAritmatika); //hasilnya 2
+
+penampungHasilOperatorAritmatika = angka1 % angka2;
+console.log(penampungHasilOperatorAritmatika); //hasilnya 10
+
+penampungHasilOperatorAritmatika = angka1 * angka2;
+console.log(penampungHasilOperatorAritmatika); //hasilnya 200
