f4f63caff328f9b72aa5697321dd482e187be6e8
Ihsan Amri Muyassar <ihsan.muyassar@binus.ac.id>
2023-09-24 17:06:18 +0700

    learn to read error

--- Minggu-1/basic1.js
+++ Minggu-1/basic1.js
@@ -27,10 +27,34 @@ penampungHasilOperatorAritmatika = angka1 - angka2;
 console.log(penampungHasilOperatorAritmatika); //hasilnya -10
 
 penampungHasilOperatorAritmatika = angka1 / angka2;
-console.log(penampungHasilOperatorAritmatika); //hasilnya 2
+console.log(penampungHasilOperatorAritmatika); //hasilnya 0.5
 
 penampungHasilOperatorAritmatika = angka1 % angka2;
 console.log(penampungHasilOperatorAritmatika); //hasilnya 10
 
 penampungHasilOperatorAritmatika = angka1 * angka2;
 console.log(penampungHasilOperatorAritmatika); //hasilnya 200
+
+//string juga bisa ditambah tapi tidak bisa dikurangi dengan operator aritmatika
+let firstName = "Ihsan";
+let lastName = "Amri";
+console.log(firstName + " " + lastName); //hasilnya Ihsan Amri
+
+/*alur pembacaan kode
+untuk membaca kode, dibacanya dari kiri ke kanan, dan dari atas ke bawah
+CONTOH:
+*/
+
+let number = 10;
+console.log(number + 10); //20
+number = 20;
+console.log(number + 10); //30 karena isi dari variabel number diperbaharui valuenya setelah console.log sebelumnya
+number = true;
+console.log(number + 10); //11 karena true = 1 sedangkan false = 0
+
+//membaca error
+const namaHewan = "Kucing";
+console.log(namaHewan);
+// namaHewan = "anjing"; -> akan error karena variabel const (konstan) tidak bisa diubah valuenya
+
+// cara buat komentar dengan cepat bisa dengan ctrl + / di line kode yang mau dikomen
